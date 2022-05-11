fa3f29389940eef84d3e5cca21fe155b5e640695
Malik Hammad Hafeez <hafeezhammad34@gmail.com>
2022-05-11 13:18:02 +0500

    3rd commit

--- circularList.cpp
+++ circularList.cpp
@@ -31,6 +31,12 @@ public:
     cirList();
     cirList(Node *newNode);
     Node *ifNodeLies(int k);
+    void appending(Node *newNode);
+    void prepending(Node *newNode);
+    void inside(int k, Node *newNode);
+    void deleting(int k);
+    void updating(int k);
+    void display();
 };
 
 cirList::cirList()
@@ -67,6 +73,106 @@ Node *cirList::ifNodeLies(int k)
 
         return temp;
     }
+}
+
+void cirList::appending(Node *newNode)
+{
+    if (ifNodeLies(newNode->key) != NULL)
+    {
+        cout << "Node exists with same key value " << newNode->key << "try another key value" << endl;
+    }
+    else
+    {
+        if (head == NULL)
+        {
+            head = newNode;
+            newNode->next = head;
+            cout << "Node Appended" << endl;
+        }
+        else
+        {
+            Node *ptr = head;
+            while (ptr != head)
+            {
+                ptr = ptr->next;
+            }
+            ptr->next = newNode;
+            newNode->next = head;
+            cout << "Node Appended" << endl;
+        }
+    }
+}
+
+void cirList::prepending(Node *newNode)
+{
+    if (ifNodeLies(newNode->key) != NULL)
+    {
+        cout << "Node exists with same key value " << newNode->key << "try another key value" << endl;
+    }
+    else
+    {
+        if (head == NULL)
+        {
+            head = newNode;
+            newNode->next = head;
+            cout << "Node prepended" << endl;
+        }
+        else
+        {
+            Node *ptr = head;
+            while (ptr != head)
+            {
+                ptr = ptr->next;
+            }
+            newNode->next = head;
+            head = newNode;
+            cout << "Node prepended" << endl;
+        }
+    }
+}
+
+void cirList::inside(int k, Node *newNode)
+{
+    Node *ptr = ifNodeLies(k);
+    if (ptr == NULL)
+    {
+        cout << " No Node exists with the key value " << k << endl;
+    }
+    else
+    {
+        if (ifNodeLies(newNode->key) != NULL)
+        {
+            cout << "Node exists with same key value " << newNode->key << "try another key value" << endl;
+        }
+        else
+        {
+            if (ptr->next == head)
+            {
+                newNode->next = head;
+                ptr->next = newNode;
+                cout << "Node Inserted at the end" << endl;
+            }
+            else
+            {
+                newNode->next = ptr->next;
+                ptr->next = newNode;
+                cout << "Node Inserted in between" << endl;
+            }
+        }
+    }
+}
+
+void cirList::deleting(int k)
+{
+    
+}
+
+void cirList::updating(int k)
+{
+}
+
+void cirList::display()
+{
 }
 
 int main()
